8a3ca686b9ea77cde796c8541592bbbc85b404ae
MonsterWJ <monsterwujie@gmail.com>
2021-06-01 18:17:50 +0800

    20210601

--- PTA/src/BasicLevelPractice/Main1007.java
+++ PTA/src/BasicLevelPractice/Main1007.java
@@ -5,20 +5,22 @@ import java.util.Scanner;
 public class Main1007 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numberMax = sc.nextInt();
-        boolean[] bo=new boolean[numberMax];
+        int number = sc.nextInt();
+        number = (number % 2 == 0) ? number - 1: number;
+        boolean[] bool = new boolean[number+2];
         int count = 0;
-        for (int i = 3; i <= numberMax; i += 2) {
-            if (isPrime(i) ){
-                bo[i-1]=true;
-                if(bo[i-3]) count++;
+        while (number > 2) {
+            if (isPrime(number)) {
+                bool[number - 1] = true;
+                if (bool[number +1]) count++;
             }
+            number-=2;
         }
         System.out.println(count);
     }
 
     public static boolean isPrime(int number) {
-        for (int divisor = 2; divisor <= Math.sqrt(number); divisor++) {
+        for (int divisor = 3; divisor <= Math.sqrt(number); divisor += 2) {
             if (number % divisor == 0) return false;
         }
         return true;
