e791b2623ac9204baf9dc6be19bac0f410ecd33f
MonsterWJ <monsterwujie@gmail.com>
2021-06-10 22:19:21 +0800

    20210610

--- PTA/src/BasicLevelPractice/Main1013.java
+++ PTA/src/BasicLevelPractice/Main1013.java
@@ -1,7 +1,9 @@
 package BasicLevelPractice;
 
+import java.util.Scanner;
+
 public class Main1013 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
     }
 }
