639b0dc73566381320f32e60f716506d71870aaa
MonsterWJ <monsterwujie@gmail.com>
2021-06-13 23:18:06 +0800

    20210613

--- PTA/src/BasicLevelPractice/Main1013.java
+++ PTA/src/BasicLevelPractice/Main1013.java
@@ -5,10 +5,14 @@ import java.util.Scanner;
 public class Main1013 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int M=sc.nextInt();
+        int N=sc.nextInt();
     }
 
-    public boolean isPrime(int num){
-        for(int i=2;i<)
+    public boolean isPrime(int num) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
         return true;
     }
 }
