4e24361f9066250ec847521f78eb0a436464aedd
MonsterWJ <monsterwujie@gmail.com>
2021-06-16 21:03:34 +0800

    20210616

--- PTA/src/BasicLevelPractice/Main1014.java
+++ PTA/src/BasicLevelPractice/Main1014.java
@@ -1,7 +1,27 @@
 package BasicLevelPractice;
 
+import java.util.Scanner;
+
 public class Main1014 {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int DAY,HH,MM,cnt=0;
+        String[] str=new String[4];
+        for(int i=0;i<4;i++){
+            str[i]=sc.next();
+        }
+        for(int i=0;i<str[0].length();i++){
+            for(int j=0;j<str[1].length();j++){
+                if(str[0].charAt(i)==str[1].charAt(j)){
+                    if(cnt==0){
+                        DAY=str[0].charAt(i)-'A'+1;
+                        cnt++;
+                    }else {
+                        HH=
+                    }
+                }
+            }
+        }
 
     }
 }
