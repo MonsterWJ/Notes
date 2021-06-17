ebc817059799ba59f209749c8b18839a470f9686
MonsterWJ <monsterwujie@gmail.com>
2021-06-17 21:34:10 +0800

    20210617

--- PTA/src/BasicLevelPractice/Main1014.java
+++ PTA/src/BasicLevelPractice/Main1014.java
@@ -4,24 +4,35 @@ import java.util.Scanner;
 
 public class Main1014 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int DAY,HH,MM,cnt=0;
-        String[] str=new String[4];
-        for(int i=0;i<4;i++){
-            str[i]=sc.next();
+        Scanner sc = new Scanner(System.in);
+        int DAY=0, HH=0, MM=0;
+        int cnt = 0;
+        String[] str = new String[4];
+        String[] day = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+        for (int i = 0; i < 4; i++) {
+            str[i] = sc.next();
         }
-        for(int i=0;i<str[0].length();i++){
-            for(int j=0;j<str[1].length();j++){
-                if(str[0].charAt(i)==str[1].charAt(j)){
-                    if(cnt==0){
-                        DAY=str[0].charAt(i)-'A'+1;
+        for (int i = 0; i < str[0].length() && i < str[1].length(); i++) {
+            if (str[0].charAt(i) == str[1].charAt(i)&&(str[0].charAt(i)>='A'&&str[0].charAt(i)<='Z'||str[0].charAt(i)>='a'&&str[0].charAt(i)<='z')) {
+                if (cnt == 0) {
+                    if(str[0].charAt(i)>='A'&&str[0].charAt(i)<='Z'){
+                        DAY = str[0].charAt(i) - 'A';
                         cnt++;
-                    }else {
-                        HH=
                     }
+                } else {
+                    HH = str[0].charAt(i) - 'A';
+                    cnt++;
+                    break;
                 }
             }
         }
-
+        for (int i = 0; i < str[2].length()&&i < str[3].length(); i++) {
+            if (str[2].charAt(i) == str[3].charAt(i)&&(str[2].charAt(i)>='A'&&str[2].charAt(i)<='Z'||str[2].charAt(i)>='a'&&str[2].charAt(i)<='z')) {
+                MM = i;
+                break;
+            }
+        }
+        if(MM>=10) System.out.println(day[DAY]+" "+(10+HH)+":"+MM);
+        else System.out.println(day[DAY]+" "+(10+HH)+":0"+MM);
     }
 }
