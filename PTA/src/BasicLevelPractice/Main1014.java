9f5ca101655d1e87872cacbc3495edaf6363cf86
MonsterWJ <monsterwujie@gmail.com>
2021-06-18 22:30:01 +0800

    20210618

--- PTA/src/BasicLevelPractice/Main1014.java
+++ PTA/src/BasicLevelPractice/Main1014.java
@@ -5,34 +5,48 @@ import java.util.Scanner;
 public class Main1014 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int DAY=0, HH=0, MM=0;
-        int cnt = 0;
         String[] str = new String[4];
         String[] day = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
         for (int i = 0; i < 4; i++) {
             str[i] = sc.next();
         }
-        for (int i = 0; i < str[0].length() && i < str[1].length(); i++) {
-            if (str[0].charAt(i) == str[1].charAt(i)&&(str[0].charAt(i)>='A'&&str[0].charAt(i)<='Z'||str[0].charAt(i)>='a'&&str[0].charAt(i)<='z')) {
-                if (cnt == 0) {
-                    if(str[0].charAt(i)>='A'&&str[0].charAt(i)<='Z'){
-                        DAY = str[0].charAt(i) - 'A';
-                        cnt++;
-                    }
-                } else {
-                    HH = str[0].charAt(i) - 'A';
+        for (int i = 0, cnt = 0; i < str[0].length() && i < str[1].length(); i++) {
+            if (str[0].charAt(i) == str[1].charAt(i)) {
+                if (cnt == 0 && str[0].charAt(i) >='A' && str[0].charAt(i) <= 'G') {
+                    System.out.print(day[str[0].charAt(i) - 'A'] + " ");
                     cnt++;
-                    break;
+                } else if (cnt == 1) {
+                    if (isDigit(str[0].charAt(i))) {
+                        System.out.print("0" + str[0].charAt(i));
+                        break;
+                    } else if (str[0].charAt(i) >= 'A' && str[0].charAt(i) <= 'N') {
+                        System.out.print(10 + (str[0].charAt(i) - 'A'));
+                        break;
+                    }
                 }
             }
         }
-        for (int i = 0; i < str[2].length()&&i < str[3].length(); i++) {
-            if (str[2].charAt(i) == str[3].charAt(i)&&(str[2].charAt(i)>='A'&&str[2].charAt(i)<='Z'||str[2].charAt(i)>='a'&&str[2].charAt(i)<='z')) {
-                MM = i;
+        for (int i = 0; i < str[2].length() && i < str[3].length(); i++) {
+            if (str[2].charAt(i) == str[3].charAt(i) && isLetter(str[2].charAt(i))) {
+                if (i < 10) {
+                    System.out.print(":0" + i);
+                } else {
+                    System.out.print(":" + i);
+                }
                 break;
             }
         }
-        if(MM>=10) System.out.println(day[DAY]+" "+(10+HH)+":"+MM);
-        else System.out.println(day[DAY]+" "+(10+HH)+":0"+MM);
+    }
+
+    public static boolean isDigit(char ch) {
+        if (ch >= '0' && ch <= '9')
+            return true;
+        else return false;
+    }
+
+    public static boolean isLetter(char ch) {
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+            return true;
+        else return false;
     }
 }
