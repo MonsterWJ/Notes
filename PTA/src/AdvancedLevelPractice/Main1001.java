73b15bfedace49b56ac1697104800766ff2f50f2
MonsterWJ <monsterwujie@gmail.com>
2021-06-23 23:17:53 +0800

    20210623

--- PTA/src/AdvancedLevelPractice/Main1001.java
+++ PTA/src/AdvancedLevelPractice/Main1001.java
@@ -1,7 +1,9 @@
 package AdvancedLevelPractice;
 
+import java.util.Scanner;
+
 public class Main1001 {
     public static void main(String[] args) {
-
+        Scanner sc=new Scanner(System.in);
     }
 }
