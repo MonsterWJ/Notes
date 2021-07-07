cca5cb635fc31a1c9951a561458230a7e9d81c48
MonsterWJ <monsterwujie@gmail.com>
2021-07-07 13:34:08 +0800

    20210707

--- SwordToOffer/src/JZ31/Solution.java
+++ SwordToOffer/src/JZ31/Solution.java
@@ -2,7 +2,9 @@ package JZ31;
 
 public class Solution {
     public int NumberOf1Between1AndN_Solution(int n) {
-        if (n <= 0) return 0;
+        if (n <= 0) {
+            return 0;
+        }
         int count = 0;
         for (long i = 1; i <= n; i *= 10) {
             long diviver = i * 10;
