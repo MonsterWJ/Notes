324093e89e2599c12b450d977c454fb8ac2e9d97
MonsterWJ <monsterwujie@gmail.com>
2021-07-20 18:19:35 +0800

    20210720

--- SwordToOffer/src/JZ3/Solution.java
+++ SwordToOffer/src/JZ3/Solution.java
@@ -1,6 +1,14 @@
 package JZ3;
 
 import java.util.ArrayList;
-public class Solution {
 
+public class Solution {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> list = new ArrayList<>();
+        while (listNode != null) {
+            list.add(0, listNode.val);
+            listNode = listNode.next;
+        }
+        return list;
+    }
 }
